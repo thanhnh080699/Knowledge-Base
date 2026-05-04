924fad65b704b34299124db17d5099151fa5aa0d
Nguyen Huy Thanh <nguyenhuythanh0806@gmail.com>
2026-05-04 16:39:35 +0700

    fix: use .d.js extension for registry schema import in test bootstrap

--- api/tests/bootstrap.ts
+++ api/tests/bootstrap.ts
@@ -7,7 +7,7 @@ import { dbAssertions } from '@adonisjs/lucid/plugins/db'
 import testUtils from '@adonisjs/core/services/test_utils'
 import { authApiClient } from '@adonisjs/auth/plugins/api_client'
 import { sessionApiClient } from '@adonisjs/session/plugins/api_client'
-import type { Registry } from '../.adonisjs/client/registry/schema.ts'
+import type { Registry } from '../.adonisjs/client/registry/schema.d.js'
 
 /**
  * This file is imported by the "bin/test.ts" entrypoint file
